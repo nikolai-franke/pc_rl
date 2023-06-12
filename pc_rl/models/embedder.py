f7876fc28e5f24d8d820d9ccecad5b18b5fca4f1
Nikolai Franke <nikolai.franke@gmail.com>
2023-06-12 08:22:26 +0200

    Moved embedder to embedder.py. Added original Point-MAE model to test file

--- pc_rl/models/embedder.py
+++ pc_rl/models/embedder.py
@@ -2,11 +2,46 @@ from typing import Callable, Optional, Union
 
 import torch
 from torch import Tensor
+from torch_geometric.nn import MLP, fps, knn
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.nn.inits import reset
 from torch_geometric.typing import (Adj, OptTensor, PairOptTensor, PairTensor,
                                     SparseTensor, torch_sparse)
 from torch_geometric.utils import add_self_loops, remove_self_loops
+
+
+class Embedder(torch.nn.Module):
+    def __init__(
+        self,
+        sampling_ratio: float,
+        neighborhood_size: int,
+        embedding_size: int,
+        random_start: bool = True,  # setting this to False is useful for testing purposes
+    ) -> None:
+        super().__init__()
+        self.sampling_ratio = sampling_ratio
+        self.neighborhood_size = neighborhood_size
+        self.random_start = random_start
+        mlp_1 = MLP([3, 128, 256])
+        mlp_2 = MLP([512, 512, embedding_size])
+        self.conv = PointConv(
+            mlp_1, mlp_2, neighborhood_size=neighborhood_size, add_self_loops=False
+        )
+
+    def forward(self, x, pos, batch):
+        # get indices of group center points via furthest point sampling
+        seed_idx = fps(
+            pos, batch, ratio=self.sampling_ratio, random_start=self.random_start
+        )
+        seeds = pos[seed_idx]
+        batch_y = batch[seed_idx]
+        # calculate neighborhood via KNN
+        from_idx, to_idx = knn(
+            pos, seeds, self.neighborhood_size, batch_x=batch, batch_y=batch_y
+        )
+        edges = torch.stack([to_idx, from_idx], dim=0)
+        x = self.conv((None, None), (pos, pos[seed_idx]), edges)
+        return x
 
 
 class PointConv(MessagePassing):
--- tests/test_mae.py
+++ tests/test_mae.py
@@ -4,7 +4,7 @@ import torch_geometric
 from knn_cuda import KNN
 from pointnet2_ops import pointnet2_utils
 
-from pc_rl.models.point_mae import Embedder
+from pc_rl.models.embedder import Embedder
 
 
 def custom_fps(data, number):
@@ -91,6 +91,343 @@ class Group(nn.Module):  # FPS + KNN
         return neighborhood, center
 
 
+## Transformers
+class Mlp(nn.Module):
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        act_layer=nn.GELU,
+        drop=0.0,
+    ):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        num_heads=8,
+        qkv_bias=False,
+        qk_scale=None,
+        attn_drop=0.0,
+        proj_drop=0.0,
+    ):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim**-0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = (
+            self.qkv(x)
+            .reshape(B, N, 3, self.num_heads, C // self.num_heads)
+            .permute(2, 0, 3, 1, 4)
+        )
+        q, k, v = (
+            qkv[0],
+            qkv[1],
+            qkv[2],
+        )  # make torchscript happy (cannot use tensor as tuple)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(
+        self,
+        dim,
+        num_heads,
+        mlp_ratio=4.0,
+        qkv_bias=False,
+        qk_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        act_layer=nn.GELU,
+        norm_layer=nn.LayerNorm,
+    ):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(
+            in_features=dim,
+            hidden_features=mlp_hidden_dim,
+            act_layer=act_layer,
+            drop=drop,
+        )
+
+        self.attn = Attention(
+            dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            attn_drop=attn_drop,
+            proj_drop=drop,
+        )
+
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(
+        self,
+        embed_dim=768,
+        depth=4,
+        num_heads=12,
+        mlp_ratio=4.0,
+        qkv_bias=False,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.0,
+    ):
+        super().__init__()
+
+        self.blocks = nn.ModuleList(
+            [
+                Block(
+                    dim=embed_dim,
+                    num_heads=num_heads,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    qk_scale=qk_scale,
+                    drop=drop_rate,
+                    attn_drop=attn_drop_rate,
+                    drop_path=drop_path_rate[i]
+                    if isinstance(drop_path_rate, list)
+                    else drop_path_rate,
+                )
+                for i in range(depth)
+            ]
+        )
+
+    def forward(self, x, pos):
+        for _, block in enumerate(self.blocks):
+            x = block(x + pos)
+        return x
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(
+        self,
+        embed_dim=384,
+        depth=4,
+        num_heads=6,
+        mlp_ratio=4.0,
+        qkv_bias=False,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.1,
+        norm_layer=nn.LayerNorm,
+    ):
+        super().__init__()
+        self.blocks = nn.ModuleList(
+            [
+                Block(
+                    dim=embed_dim,
+                    num_heads=num_heads,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    qk_scale=qk_scale,
+                    drop=drop_rate,
+                    attn_drop=attn_drop_rate,
+                    drop_path=drop_path_rate[i]
+                    if isinstance(drop_path_rate, list)
+                    else drop_path_rate,
+                )
+                for i in range(depth)
+            ]
+        )
+        self.norm = norm_layer(embed_dim)
+        self.head = nn.Identity()
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def forward(self, x, pos, return_token_num):
+        for _, block in enumerate(self.blocks):
+            x = block(x + pos)
+
+        x = self.head(
+            self.norm(x[:, -return_token_num:])
+        )  # only return the mask tokens predict pixel
+        return x
+
+
+# Pretrain model
+class MaskTransformer(nn.Module):
+    def __init__(self, config, **kwargs):
+        super().__init__()
+        self.config = config
+        # define the transformer argparse
+        self.mask_ratio = config.transformer_config.mask_ratio
+        self.trans_dim = config.transformer_config.trans_dim
+        self.depth = config.transformer_config.depth
+        self.drop_path_rate = config.transformer_config.drop_path_rate
+        self.num_heads = config.transformer_config.num_heads
+        print_log(f"[args] {config.transformer_config}", logger="Transformer")
+        # embedding
+        self.encoder_dims = config.transformer_config.encoder_dims
+        self.encoder = Encoder(encoder_channel=self.encoder_dims)
+
+        self.mask_type = config.transformer_config.mask_type
+
+        self.pos_embed = nn.Sequential(
+            nn.Linear(3, 128),
+            nn.GELU(),
+            nn.Linear(128, self.trans_dim),
+        )
+
+        dpr = [x.item() for x in torch.linspace(0, self.drop_path_rate, self.depth)]
+        self.blocks = TransformerEncoder(
+            embed_dim=self.trans_dim,
+            depth=self.depth,
+            drop_path_rate=dpr,
+            num_heads=self.num_heads,
+        )
+
+        self.norm = nn.LayerNorm(self.trans_dim)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=0.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv1d):
+            trunc_normal_(m.weight, std=0.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def _mask_center_block(self, center, noaug=False):
+        """
+        center : B G 3
+        --------------
+        mask : B G (bool)
+        """
+        # skip the mask
+        if noaug or self.mask_ratio == 0:
+            return torch.zeros(center.shape[:2]).bool()
+        # mask a continuous part
+        mask_idx = []
+        for points in center:
+            # G 3
+            points = points.unsqueeze(0)  # 1 G 3
+            index = random.randint(0, points.size(1) - 1)
+            distance_matrix = torch.norm(
+                points[:, index].reshape(1, 1, 3) - points, p=2, dim=-1
+            )  # 1 1 3 - 1 G 3 -> 1 G
+
+            idx = torch.argsort(distance_matrix, dim=-1, descending=False)[0]  # G
+            ratio = self.mask_ratio
+            mask_num = int(ratio * len(idx))
+            mask = torch.zeros(len(idx))
+            mask[idx[:mask_num]] = 1
+            mask_idx.append(mask.bool())
+
+        bool_masked_pos = torch.stack(mask_idx).to(center.device)  # B G
+
+        return bool_masked_pos
+
+    def _mask_center_rand(self, center, noaug=False):
+        """
+        center : B G 3
+        --------------
+        mask : B G (bool)
+        """
+        B, G, _ = center.shape
+        # skip the mask
+        if noaug or self.mask_ratio == 0:
+            return torch.zeros(center.shape[:2]).bool()
+
+        self.num_mask = int(self.mask_ratio * G)
+
+        overall_mask = np.zeros([B, G])
+        for i in range(B):
+            mask = np.hstack(
+                [
+                    np.zeros(G - self.num_mask),
+                    np.ones(self.num_mask),
+                ]
+            )
+            np.random.shuffle(mask)
+            overall_mask[i, :] = mask
+        overall_mask = torch.from_numpy(overall_mask).to(torch.bool)
+
+        return overall_mask.to(center.device)  # B G
+
+    def forward(self, neighborhood, center, noaug=False):
+        # generate mask
+        if self.mask_type == "rand":
+            bool_masked_pos = self._mask_center_rand(center, noaug=noaug)  # B G
+        else:
+            bool_masked_pos = self._mask_center_block(center, noaug=noaug)
+
+        group_input_tokens = self.encoder(neighborhood)  #  B G C
+
+        batch_size, seq_len, C = group_input_tokens.size()
+
+        x_vis = group_input_tokens[~bool_masked_pos].reshape(batch_size, -1, C)
+        # add pos embedding
+        # mask pos center
+        masked_center = center[~bool_masked_pos].reshape(batch_size, -1, 3)
+        pos = self.pos_embed(masked_center)
+
+        # transformer
+        x_vis = self.blocks(x_vis, pos)
+        x_vis = self.norm(x_vis)
+
+        return x_vis, bool_masked_pos
+
+
 def init_layers(layers):
     for layer in layers:
         if isinstance(layer, torch.nn.modules.conv.Conv1d) or isinstance(
@@ -133,3 +470,7 @@ def test_embedding():
     out_2 = embedder.forward(None, input_tensor_2, batch)
 
     assert torch.allclose(out_1.reshape(-1, embedding_size), out_2)
+
+
+if __name__ == "__main__":
+    test_embedding()
