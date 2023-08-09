411023caa3bc9cdc497c721e8c6e9b1538b200ad
Nikolai Franke <nikolai.franke@gmail.com>
2023-08-09 12:06:04 +0200

    Added in memory dataset.

---
+++ pc_rl/datasets/in_memory.py
@@ -0,0 +1,34 @@
+import pathlib
+
+import numpy as np
+import torch
+from torch_geometric.data import InMemoryDataset
+
+
+class PcInMemoryDataset(InMemoryDataset):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+        self.file_paths = [f for f in pathlib.Path(root).iterdir() if f.is_file()]
+        print(self.filenames)
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        return ["000000.npy", "000001.npy", ...]
+
+    @property
+    def processed_file_names(self):
+        return ["data.pt"]
+
+    def process(self):
+        data_list = [np.load(file_path) for file_path in self.file_paths]
+        
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+
