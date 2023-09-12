070eb6ace9755b73e1d75b5d206a745ffec416bc
Nikolai Franke <nikolai.franke@gmail.com>
2023-09-12 15:40:10 +0200

    Replaced classification token with sequence pooling

--- pc_rl/models/finetune_encoder.py
+++ pc_rl/models/finetune_encoder.py
@@ -12,6 +12,7 @@ class FinetuneEncoder(nn.Module):
         self,
         transformer_encoder: TransformerEncoder,
         pos_embedder: nn.Module,
+        # out_dim: int,
     ) -> None:
         super().__init__()
         self.transformer_encoder = transformer_encoder
@@ -19,18 +20,12 @@ class FinetuneEncoder(nn.Module):
         self.dim = self.transformer_encoder.dim
         self.norm = nn.LayerNorm(self.dim)
         self.attention_pool = nn.Linear(self.dim, 1)
-        # self.cls_token = nn.Parameter(torch.zeros(1, 1, self.dim))
-        # self.cls_pos = nn.Parameter(torch.randn(1, 1, self.dim))
 
     def forward(self, x, center_points, _=None):
-        # cls_tokens = self.cls_token.expand(x.shape[0], -1, -1)
-        # cls_pos = self.cls_pos.expand(x.shape[0], -1, -1)
         pos = self.pos_embedder(center_points)
-        # pos = torch.cat((cls_pos, pos), dim=1)
-        # x = torch.cat((cls_tokens, x), dim=1)
         x = self.transformer_encoder(x, pos)
         x = self.norm(x)
-        x = torch.matmul(F.softmax(self.attention_pool(x), dim=1).transpose(-1, -2), x).squeeze(-2)
-        # out = torch.cat([x[:, 0], x[:, 1:].max(1)[0]], dim=-1)
-        # return x[:, 0]
+        x = torch.matmul(
+            F.softmax(self.attention_pool(x), dim=1).transpose(-1, -2), x
+        ).squeeze(-2)
         return x
