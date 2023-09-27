ac8f4709f24742855fb25dfe800e6033a3620601
Nikolai Franke <nikolai.franke@gmail.com>
2023-09-27 14:49:32 +0200

    Fixed dict_to_batched_data

--- pc_rl/utils/array_dict.py
+++ pc_rl/utils/array_dict.py
@@ -8,12 +8,12 @@ from torch import Tensor
 def dict_to_batched_data(
     array_dict: ArrayDict[Tensor],
 ) -> tuple[Tensor, Tensor, Tensor | None]:
-    pos, ptr = array_dict["pos"], array_dict["ptr"]
+    points, ptr = array_dict["pos"], array_dict["ptr"]
     num_nodes = ptr[1:] - ptr[:-1]
-    features = pos[3:] if pos.shape[-1] > 3 else None
+    pos = points[..., :3]
+    features = points[..., 3:] if points.shape[-1] > 3 else None
     batch = torch.repeat_interleave(
         torch.arange(len(num_nodes), device=num_nodes.device),
         repeats=num_nodes,
     )
-
     return pos, batch, features
