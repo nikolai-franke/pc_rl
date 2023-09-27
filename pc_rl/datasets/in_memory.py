4ee959b6414ea8f01f2009850679c5f4614561fb
Nikolai Franke <nikolai.franke@gmail.com>
2023-09-27 10:04:37 +0200

    Added color dataset for pretraining

--- pc_rl/datasets/in_memory.py
+++ pc_rl/datasets/in_memory.py
@@ -36,3 +36,33 @@ class PcInMemoryDataset(InMemoryDataset):
 
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
+
+
+class ColorPcInMemoryDataset(InMemoryDataset):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+        self.file_paths = [f for f in pathlib.Path(root).iterdir() if f.is_file()]
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        return ["000000.npy", "000001.npy", ...]
+
+    @property
+    def processed_file_names(self):
+        return ["data.pt"]
+
+    def process(self):
+        data_list = []
+        for file_path in self.file_paths:
+            data = torch.from_numpy(np.load(file_path)).float()
+            data_list.append(Data(pos=data[:, :3], x=data[:, 3:]))
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
