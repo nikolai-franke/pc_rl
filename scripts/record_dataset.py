12f8b250a3da51f3853166129b18c5cc46ad2bb8
Nikolai Franke <nikolai.franke@gmail.com>
2023-10-20 07:10:35 +0200

    Added command line argument to save csv to record dataset

--- scripts/record_dataset.py
+++ scripts/record_dataset.py
@@ -21,15 +21,20 @@ def main(config: DictConfig):
     os.makedirs(save_path, exist_ok=True)
     finished = False
     obs_counter = 0
+    save_txt = config.get("save_txt", False)
     while not finished:
         done = False
         env.reset()
         while not done:
             obs, _, terminated, truncated, *_ = env.step(env.action_space.sample())
-            # np.save(save_path / str(obs_counter).zfill(6), obs)
-            np.savetxt(
-                save_path / (str(obs_counter).zfill(6) + ".csv"), obs, delimiter=","
-            )
+
+            if save_txt:
+                np.savetxt(
+                    save_path / (str(obs_counter).zfill(6) + ".csv"), obs, delimiter=","
+                )
+            else:
+                np.save(save_path / str(obs_counter).zfill(6), obs)
+
             done = terminated or truncated
 
             obs_counter += 1
