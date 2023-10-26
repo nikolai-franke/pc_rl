1c64c06974caa5375f745fbb90c93b3f1b6786b3
Nikolai Franke <nikolai.franke@gmail.com>
2023-10-26 13:45:40 +0200

    Added random rotations to AUX SAC.

--- pc_rl/agents/aux_sac.py
+++ pc_rl/agents/aux_sac.py
@@ -4,9 +4,20 @@ import torch
 from parllel import ArrayTree
 from parllel.torch.distributions.squashed_gaussian import SquashedGaussian
 from torch import Tensor
+from torch_geometric.data import Batch, Data
+from torch_geometric.transforms import Compose, NormalizeScale, RandomRotate
 
 from pc_rl.agents.sac import PcSacAgent
 from pc_rl.utils.array_dict import dict_to_batched_data
+
+transform = Compose(
+    [
+        RandomRotate(180, axis=0),
+        RandomRotate(180, axis=1),
+        RandomRotate(180, axis=2),
+        NormalizeScale(),
+    ]
+)
 
 
 class AuxPcSacAgent(PcSacAgent):
@@ -28,6 +39,9 @@ class AuxPcSacAgent(PcSacAgent):
 
     def auto_encoder(self, observation: ArrayTree[Tensor]) -> tuple[Tensor, Tensor]:
         pos, batch, color = dict_to_batched_data(observation)
+        data = Data(pos=pos, x=color)
+        data = transform(data)
+        pos, color = data.pos, data.x
         x, neighborhoods, center_points = self.model["tokenizer"](pos, batch, color)
         prediction, ground_truth = self.model["rl_mae"](x, neighborhoods, center_points)
         return prediction, ground_truth
