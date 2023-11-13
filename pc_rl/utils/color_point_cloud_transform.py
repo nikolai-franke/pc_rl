c1f7893b6edeed154c7292e7b5452ccdb6cdb3f8
Nikolai Franke <nikolai.franke@gmail.com>
2023-11-13 09:35:17 +0100

    Small error fix

--- pc_rl/utils/color_point_cloud_transform.py
+++ pc_rl/utils/color_point_cloud_transform.py
@@ -11,6 +11,8 @@ class ColorPointCloud(BaseTransform):
     def __call__(self, data: Data) -> Data:
         assert data.x == None
         new_color = np.random.random(self.additional_channels).astype(np.float32)
-        full_color = np.broadcast_to(new_color, data.pos.shape)
+        full_color = np.broadcast_to(
+            new_color, (data.pos.shape[0], self.additional_channels)
+        )
         data.x = torch.tensor(full_color)
         return data
