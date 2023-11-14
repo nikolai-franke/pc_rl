b07e1203c4a0da4cd7aefa83852b4b90b546d732
Nikolai Franke <nikolai.franke@gmail.com>
2023-11-14 15:55:00 +0100

    Return to infinite horizon task

--- pc_rl/envs/mani_skill_env/push_chair.py
+++ pc_rl/envs/mani_skill_env/push_chair.py
@@ -70,8 +70,8 @@ class PushChair(PushChairEnv):
                     if chair_vel_norm <= 0.1 and chair_ang_vel_norm <= 0.2:
                         stage_reward += 2
 
-                    if info["success"]:
-                        stage_reward = 50
+                    # if info["success"]:
+                    #     stage_reward = 50
                 else:
                     # Try to increase velocity along direction to the target
                     # Compute directional velocity
