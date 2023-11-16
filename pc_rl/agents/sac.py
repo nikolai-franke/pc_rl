5b3d56c21e07edc313ec8182097c108b5fe9748d
Nikolai Franke <nikolai.franke@gmail.com>
2023-11-16 18:21:41 +0100

    Updated agent for use with state

--- pc_rl/agents/sac.py
+++ pc_rl/agents/sac.py
@@ -20,24 +20,40 @@ class PcSacAgent(SacAgent):
         device: torch.device,
         learning_starts: int = 0,
         pretrain_std: float = 0.75,  # With squash 0.75 is near uniform.
+        with_robot_state: bool = False,
     ) -> None:
         super().__init__(model, distribution, device, learning_starts, pretrain_std)
         model["target_tokenizer"] = copy.deepcopy(model["tokenizer"])
         model["target_tokenizer"].requires_grad_(False)
         model["target_encoder"] = copy.deepcopy(model["encoder"])
         model["target_encoder"].requires_grad_(False)
+        self.with_robot_state = with_robot_state
 
-    def encode(self, observation: ArrayTree[Tensor]) -> Tensor:
-        pos, batch, color = dict_to_batched_data(observation)
+    def _encode_point_cloud(self, point_cloud: ArrayTree[Tensor]) -> Tensor:
+        pos, batch, color = dict_to_batched_data(point_cloud)
         x, _, center_points = self.model["tokenizer"](pos, batch, color)
         encoder_out = self.model["encoder"](x, center_points)
         return encoder_out
 
-    def target_encode(self, observation: ArrayTree[Tensor]) -> Tensor:
-        pos, batch, color = dict_to_batched_data(observation)
+    def _target_encode_point_cloud(self, point_cloud: ArrayTree[Tensor]) -> Tensor:
+        pos, batch, color = dict_to_batched_data(point_cloud)
         x, _, center_points = self.model["target_tokenizer"](pos, batch, color)
         encoder_out = self.model["target_encoder"](x, center_points)
         return encoder_out
+
+    def encode(self, observation: ArrayTree[Tensor]) -> Tensor:
+        if not self.with_robot_state:
+            return self._encode_point_cloud(observation)
+
+        encoder_out = self._encode_point_cloud(observation["point_cloud"])
+        return torch.concatenate([encoder_out, observation["state"]], dim=-1)
+
+    def target_encode(self, observation: ArrayTree[Tensor]) -> Tensor:
+        if not self.with_robot_state:
+            return self._target_encode_point_cloud(observation)
+
+        encoder_out = self._target_encode_point_cloud(observation["point_cloud"])
+        return torch.concatenate([encoder_out, observation["state"]], dim=-1)
 
     def update_target(self, tau: float | int = 1) -> None:
         super().update_target(tau)
