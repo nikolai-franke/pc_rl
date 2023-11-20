a7091555993996408b9e03cc3f135856478d129f
Nikolai Franke <nikolai.franke@gmail.com>
2023-11-20 19:46:52 +0100

    Improved sinusoidal pos embedder

--- pc_rl/models/modules/sin_pos_embedder.py
+++ pc_rl/models/modules/sin_pos_embedder.py
@@ -19,20 +19,27 @@ class SinusoidalPosEmbedder(nn.Module):
         self.temperature = temperature
         self.padding = token_dim - self.num_pos_features * self.n_dim
         self.scale = scale * 2 * math.pi
+        self.token_dim = token_dim
 
     def forward(self, xyz: torch.Tensor) -> torch.Tensor:
         assert xyz.shape[-1] == self.n_dim
-        dim_t = torch.arange(
-            self.num_pos_features, dtype=torch.float32, device=xyz.device
-        )
-        dim_t = self.temperature ** (
-            2 * torch.div(dim_t, 2, rounding_mode="trunc") / self.num_pos_features
+        B, G, _ = xyz.shape
+        dim_t = torch.pow(
+            self.temperature,
+            2
+            * torch.arange(0, self.num_pos_features // 2, device=xyz.device)
+            / self.num_pos_features,
         )
         xyz = xyz * self.scale
         pos_divided = xyz.unsqueeze(-1) / dim_t
-        pos_sin = pos_divided[..., 0::2].sin()
-        pos_cos = pos_divided[..., 1::2].cos()
-        pos_emb = torch.stack([pos_sin, pos_cos], dim=-1).reshape(*xyz.shape[:-1], -1)
+
+        pos_sin = pos_divided.sin()
+        pos_cos = pos_divided.cos()
+        pos_emb = torch.zeros(
+            B, G, self.token_dim, dtype=torch.float32, device=xyz.device
+        )
+        pos_emb[..., 0::2] = pos_sin.reshape(B, G, -1)
+        pos_emb[..., 1::2] = pos_cos.reshape(B, G, -1)
 
         pos_emb = F.pad(pos_emb, (0, self.padding))
         return pos_emb
