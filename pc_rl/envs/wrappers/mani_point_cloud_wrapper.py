Fixed wrong index

pc_rl/envs/wrappers/mani_point_cloud_wrapper.py
@@ -209,7 +209,7 @@ class FrameStackWrapper(gym.ObservationWrapper):
 
     def observation(self, observation):
         for i, frame in enumerate(self.frames):
-            frame[..., -1] = (i + 1) / self.num_frames
+            frame[..., -1] = i
 
         return np.concatenate(self.frames)
 