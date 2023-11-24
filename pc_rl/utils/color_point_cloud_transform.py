a9d9f17a4f585214128df6b9d6c26b4b40af9ed5
Nikolai Franke <nikolai.franke@gmail.com>
2023-11-24 09:00:23 +0100

    Added possibility to add random colors to point cloud

--- pc_rl/utils/color_point_cloud_transform.py
+++ pc_rl/utils/color_point_cloud_transform.py
@@ -5,12 +5,16 @@ from torch_geometric.transforms import BaseTransform
 
 
 class AddChannels(BaseTransform):
-    def __init__(self, additional_channels: int) -> None:
+    def __init__(self, additional_channels: int, randomize: bool = False) -> None:
         self.additional_channels = additional_channels
+        self.randomize = randomize
 
     def __call__(self, data: Data) -> Data:
         assert data.x == None
-        new_channel = np.zeros(self.additional_channels).astype(np.float32)
+        if self.randomize:
+            new_channel = np.random.random(self.additional_channels).astype(np.float32)
+        else:
+            new_channel = np.zeros(self.additional_channels).astype(np.float32)
         full_channel = np.broadcast_to(
             new_channel, (data.pos.shape[0], self.additional_channels)
         )
